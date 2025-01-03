2cff959656b068a5b31cc14649024b4c625b9851
albgarre <garre.alberto@gmail.com>
2025-01-03 17:16:19 +0100

    Changed the calculations to ln CFU/h
    I was trying to use the same units as the original paper, but it was just causing confusion.

--- fit_kim.R
+++ fit_kim.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(FME)
 library(cowplot)
+library(ggsci)
 
 ## Define data (doi.org/10.5713/ajas.2013.13579)
 
@@ -57,7 +58,15 @@ pred_sqmu <- function(p, temp) {
 residuals_sqmu <- function(p, my_d) {
   p <- as.list(p)
   
-  pred_sqmu(p, my_d$temp) - my_d$sqmu
+  # pred_sqmu(p, my_d$temp) - my_d$sqmu
+  
+  ## convert to ln CFU/h
+  
+  mu_log10 <- my_d$sqmu^2
+  mu_ln <- log(10)*mu_log10
+  
+  pred_sqmu(p, my_d$temp) - sqrt(mu_ln)
+  
 
 }
 
@@ -77,8 +86,8 @@ residuals_lambda <- function(p, my_d) {
   pred_lambda(p, my_d$temp) - my_d$lambda
 }
 
-pred_lambda(c(Tmin = 0, b = .1, logC0 = 0), temp = 5:15)
-residuals_lambda(c(Tmin = 0, b = .1, logC0 = 0), d[[1]])
+# pred_lambda(c(Tmin = 0, b = .1, logC0 = 0), temp = 5:15)
+# residuals_lambda(c(Tmin = 0, b = .1, logC0 = 0), d[[1]])
 
 cost_all <- function(p, this_data,
                      weight = NULL
@@ -158,12 +167,13 @@ p <- my_models %>%
   ggplot() +
   geom_line(aes(x = temp, y = sqmu^2, colour = product),
             linetype = 2, size = 1) +
-  geom_point(aes(x = temp, y = mu, colour = product),
+  geom_point(aes(x = temp, y = mu*log(10),  # convert to ln
+                 colour = product),
              data = d %>% imap_dfr(., ~ mutate(.x, product = .y)),
              size = 5, shape = 1
   ) +
   labs(x = "Storage temperature (ºC)",
-       y = expression(Specific~growth~rate~(h^-1))
+       y = expression(Maximum~specific~growth~rate~(ln~CFU/h))
        ) +
   theme_classic(base_size = 14) +
   theme(legend.position = "none",
@@ -174,7 +184,21 @@ p <- my_models %>%
 ggsave(p, filename = "Figure_3.png",
        width = 9, height = 6)
 
-## Fitting just the growth mdoel
+## Table 2
+
+my_models %>%
+  map(., ~ summary(.)$par)
+
+d %>%
+  map2(., my_models,
+       ~ cost_all(.y$par, .x, weight = "sd")
+       ) %>%
+  map(., ~tibble(r = ., r2 = r^2)) %>%
+  map(., ~ summarize(., SSE = mean(r2))) %>%
+  map(., ~ mutate(., RMSE = sqrt(SSE)))
+
+
+## Fitting just the growth model
 
 d %>%
   map(.,
@@ -182,7 +206,9 @@ d %>%
                p = c(Tmin = 0, b = .1),
                my_d = .
       )
-  ) %>% map(summary)
+  ) %>% map(., ~summary(.)$par)
+
+
 
 
 
